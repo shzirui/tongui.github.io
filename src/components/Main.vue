9b868d74c80349c5cb6c186b82b89bf0eb79c30d
shzirui <609146431@qq.com>
2025-04-17 17:04:04 +0800

    Initial commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -128,65 +128,8 @@
           <span>Video</span>
         </a>
       </span>
-      <!-- <span class="link-block">
-     
-        <a target="_blank" href="https://huggingface.co/li-qing/llava-next-llama3-8b-student-fire/tree/main"
-          class="external-link button is-normal is-rounded is-dark">
-          <span class="icon">
-            <i class="fa fa-share-square"></i>
-          </span>
-          <span>Model</span>
-        </a>
-      </span> -->
-      <!-- <span class="link-block">
- 
-        <a target="_blank" href="https://li-qing-fire.hf.space"
-          class="external-link button is-normal is-rounded is-dark">
-          <span class="icon">
-            <i class="fa fa-images"></i>
-          </span>
-          <span>Demo</span>
-        </a>
-      </span> -->
-      <!-- <span class="link-block">
- 
-        <a target="_blank" href="https://x.com/Sealiqing/status/1819279627438973133"
-          class="external-link button is-normal is-rounded is-dark">
-          <span class="icon">
-<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="1.0em" height="1.0em" viewBox="0,0,256,256">
-<g fill="#e9e9e9" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M6.91992,6l14.2168,20.72656l-14.9082,17.27344h3.17773l13.13867,-15.22266l10.44141,15.22266h10.01367l-14.87695,-21.6875l14.08008,-16.3125h-3.17578l-12.31055,14.26172l-9.7832,-14.26172z"></path></g></g>
-</svg>
-          </span>
-          <span>Twitter</span>
-        </a>
-      </span> -->
-      <!-- <span class="link-block">
-            <a href="file/clova_cvpr24_poster.pdf"
-               class="external-link button is-normal is-rounded is-dark">
-              <span class="icon">
-                  <i class="fas fa-file-pdf"></i>
-              </span>
-              <span>Poster (CVPR'24)</span>
-            </a>
-          </span> -->
-
-      <!-- <span class="link-block">
-            <a href="file/clova_slides.pdf"
-               class="external-link button is-normal is-rounded is-dark">
-              <span class="icon">
-                  <i class="fas fa-file-pdf"></i>
-              </span>
-              <span>Slides</span>
-            </a>
-          </span> -->
 
     </div>
-
-
-    <!-- <div class="tldr">
-      <p><b>TL;DR</b> This paper proposes T3-Agent, a multi-modal agent tuned with the MM-Traj dataset for better
-        tool-usage reasoning, boosting VLM performance by 20% on benchmarks.</p>
-    </div> -->
 
     <div class="section">
       <el-card class="teaser">
@@ -215,7 +158,7 @@
       </p>
     </div>
 
-    <video width="80%" height=auto controls="./tongui/intro.png">
+    <video width="80%" height=auto controls>
       <source src="/intro.mp4" type="video/mp4">
     </video>
 
@@ -265,28 +208,6 @@ The <b>action distribution</b> is shown in (d). Our dataset encompasses a divers
         <el-image src="./tongui/dataset_statistics.png"></el-image>
       </el-card>
     </div>
-
-
-
-
-    <!-- <div class="section">
-      <div class="section-title">
-        <svg t="1735026864471" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-          p-id="2411" width="35" height="35">
-          <path
-            d="M847.40096 805.88416c8.92544 8.92544 8.92544 23.00032-0.2752 32.50048a22.95552 22.95552 0 0 1-31.97696-0.54912L674.84288 697.78304a22.72512 22.72512 0 0 1 0.5248-32.22528c8.704-8.65024 23.296-8.65024 32.52608 0l139.5072 140.3264zM785.34784 15.4624a188.09216 188.09216 0 0 1 44.17664 4.62976 195.55456 195.55456 0 0 1 43.07712 13.00096 38.23744 38.23744 0 0 1 19.776 50.10176 40.4096 40.4096 0 0 1-7.87456 11.92576c-28.97664 28.97536-57.95328 57.6768-86.70464 86.67776l4.89984 17.3248 4.32512 17.6256 17.87648 4.84992 17.3504 4.60032c28.70144-28.97536 57.7024-57.42592 86.67904-86.67776 14.62656-14.90048 39.0016-14.90048 54.17856 0a38.84544 38.84544 0 0 1 9.19936 16.256 197.32992 197.32992 0 0 1 11.648 38.47552v0.5504h0.2752a229.60768 229.60768 0 0 1 4.32512 44.40192 223.29728 223.29728 0 0 1-223.20768 222.92096c-4.32512 0-8.65024-0.2752-13.00096-0.2752l-42.25152 41.728 232.41088 232.40576 1.89952 2.1504a156.99456 156.99456 0 0 1-1.89952 220.23168l-0.5504 0.5248a156.69632 156.69632 0 0 1-110.50496 45.52064c-40.0768 0-80.9792-15.1744-111.05536-46.05056L507.45984 725.68448 269.87392 963.79136c-49.55264 49.55136-109.43104 53.9264-158.464 32.77568a155.91936 155.91936 0 0 1-50.09408-33.8752h-0.27648a158.7584 158.7584 0 0 1-33.57696-49.82656c-21.15072-49.024-16.80128-109.15328 33.024-158.48064v-0.5248L325.4016 488.95616l-96.95488-96.97792-86.12864-25.728a37.59744 37.59744 0 0 1-24.37632-21.4016L35.28704 151.16416a38.048 38.048 0 0 1 6.77504-44.40064l34.67776-34.39872 34.15168-34.1504a37.67552 37.67552 0 0 1 41.70112-8.12544l196.11008 83.72736a37.43616 37.43616 0 0 1 21.67552 24.37632l25.45152 86.12736 96.97408 96.95616 69.632-69.05216c-0.2752-4.89984-0.5248-9.50016-0.5248-13.02528h0.2496a222.2208 222.2208 0 0 1 65.28-158.17984v-0.27904a224.13952 224.13952 0 0 1 157.9072-65.28zM675.9424 557.7536l-114.3296 114.3296 232.4352 232.40704a80.75904 80.75904 0 0 0 114.304 0.2496l0.5248-0.2496c15.72608-15.17568 23.0272-36.608 23.0272-57.1776a79.96416 79.96416 0 0 0-21.952-55.5264l-1.6-1.62432-232.4096-232.40832z m-296.88832-122.45248l59.8528-59.5776-103.72992-104.00256a38.33984 38.33984 0 0 1-10.30144-18.976l-22.49984-76.12672L146.368 110.816 130.39232 126.5408l-15.97568 16.256 66.32832 156.032 78.5536 23.00032a33.73056 33.73056 0 0 1 16.2752 10.02624l103.48032 103.45344z m400.896-343.43552a145.92 145.92 0 0 0-98.60224 43.32544 145.408 145.408 0 0 0-43.32672 104.00384h0.5248v10.57536l1.0752 8.92544c2.72512 11.92448-1.0752 24.94976-10.27584 34.432L114.41664 807.76192h-0.2752c-23.82592 24.65024-26.55104 52.55168-16.80128 75.5776a93.87136 93.87136 0 0 0 17.60128 25.472 87.296 87.296 0 0 0 26.00064 17.6c23.02592 10.02496 50.65216 7.296 75.05408-16.5248l263.83744-263.8336 1.0752-1.0752 167.936-168.2048 1.0752-1.37472 80.7296-80.17792h0.2496a36.23808 36.23808 0 0 1 32.256-11.10016 79.7696 79.7696 0 0 0 11.40096 1.09952c2.6752 0.2752 6.47552 0.5248 10.80064 0.5248a148.64128 148.64128 0 0 0 104.02944-42.52544v-0.5248a145.73568 145.73568 0 0 0 42.51904-98.60224c-16.52608 16.77568-33.05216 33.024-49.57824 50.10176-9.45024 10.54976-23.82592 14.90048-38.4512 11.62496l-38.4768-10.52544-38.97728-10.57536c-13.02528-3.52512-23.57632-12.99968-27.10144-27.10016l-10.30016-38.99776-10.57536-39.0016c-2.944-11.92448-0.2752-26.55104 10.02496-36.0256 17.07648-17.32608 34.15168-34.40128 51.47776-51.72736z"
-            fill="#3B3F51" p-id="2412"></path>
-        </svg> Tools in T3-Agent
-      </div>
-      <p class="intro">
-        We deploy <b>real-executable tools</b> for the agent instead of only providing tool names. Our tools are across
-        multiple categories: web search, visual perception, image generation/editing, file understanding, multi-modal
-        understanding, and multiple Python packages.
-      </p>
-      <el-image class="stats-img" src="./stats/tools.png"></el-image>
-    </div> -->
-
-
 
     <div class="section">
       <div class="section-title">Evaluation and Results </div>
@@ -362,12 +283,9 @@ Compared with ShowUI-Qwen2.5-VL-3B that fine-tunes the Qwen2.5-VL-3B model using
     </div>
   </section>
 
-
-  <!-- <div class="footer">
-    This website is inspired by <el-link href="https://mathvista.github.io/">MathVista</el-link> and <el-link
-      href="https://nerfies.github.io/">Nerfies</el-link>.
-  </div> -->
 </template>
+
+
 
 <style scoped>
 .main {
